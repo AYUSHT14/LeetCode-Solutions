8a04a57a84f3002b672a1f398f5ff558085253b3
Ayush Tandon <162042478+AYUSHT14@users.noreply.github.com>
2026-05-04 00:43:20 +0530

    Time: 1 ms (80.56%), Space: 43.3 MB (19.95%) - LeetHub

---
+++ 1024-video-stitching/1024-video-stitching.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int videoStitching(int[][] clips, int time) {
+        Arrays.sort(clips,(a,b)->Integer.compare(a[0],b[0]));
+        int count = 0;
+        int currEnd = 0;
+        int farthest = 0;
+        int i=0;
+        int n = clips.length;
+        while(currEnd < time){
+            while(i < n && clips[i][0] <= currEnd){
+                farthest = Math.max(farthest,clips[i][1]);
+                i++;
+            }
+            if(farthest == currEnd) return -1;
+            count++;
+            currEnd = farthest;
+        }
+        return count;
+    }
+}
