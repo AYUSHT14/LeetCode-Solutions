0b658dba9ca37afa1c5516882c4160e952ccb594
Ayush Tandon <162042478+AYUSHT14@users.noreply.github.com>
2026-08-15 23:19:05 +0530

    Time: 0 ms (100%), Space: 43.5 MB (60.76%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -7,8 +7,8 @@ class Solution {
             if(nums[mid] == 0){
             int temp = nums[low];
             nums[low] = nums[mid];
-            low++;
             nums[mid] = temp;
+            low++;
             mid++;
             }
             else if(nums[mid] == 1) mid++;
